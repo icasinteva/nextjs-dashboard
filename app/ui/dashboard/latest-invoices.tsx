231c8f1015f31e59994c5db4ba2c05b1de5934b0
Iris <i.casinteva@gmail.com>
2025-01-19 22:56:02 +0100

    chapter 7

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -17,7 +17,7 @@ export default async function LatestInvoices({
       <div className='flex grow flex-col justify-between rounded-xl bg-gray-50 p-4'>
         {/* NOTE: Uncomment this code in Chapter 7 */}
 
-        {/* <div className="bg-white px-6">
+        <div className='bg-white px-6'>
           {latestInvoices.map((invoice, i) => {
             return (
               <div
@@ -26,22 +26,22 @@ export default async function LatestInvoices({
                   'flex flex-row items-center justify-between py-4',
                   {
                     'border-t': i !== 0,
-                  },
+                  }
                 )}
               >
-                <div className="flex items-center">
+                <div className='flex items-center'>
                   <Image
                     src={invoice.image_url}
                     alt={`${invoice.name}'s profile picture`}
-                    className="mr-4 rounded-full"
+                    className='mr-4 rounded-full'
                     width={32}
                     height={32}
                   />
-                  <div className="min-w-0">
-                    <p className="truncate text-sm font-semibold md:text-base">
+                  <div className='min-w-0'>
+                    <p className='truncate text-sm font-semibold md:text-base'>
                       {invoice.name}
                     </p>
-                    <p className="hidden text-sm text-gray-500 sm:block">
+                    <p className='hidden text-sm text-gray-500 sm:block'>
                       {invoice.email}
                     </p>
                   </div>
@@ -54,7 +54,7 @@ export default async function LatestInvoices({
               </div>
             );
           })}
-        </div> */}
+        </div>
         <div className='flex items-center pb-2 pt-6'>
           <ArrowPathIcon className='h-5 w-5 text-gray-500' />
           <h3 className='ml-2 text-sm text-gray-500 '>Updated just now</h3>
