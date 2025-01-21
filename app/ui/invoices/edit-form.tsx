421d213a3e9d90ab0f838f45eee6ba5c10c9487f
Iris <i.casinteva@gmail.com>
2025-01-21 14:21:55 +0100

    chapter 14

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import { State, updateInvoice } from '@/app/lib/actions';
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+import { Button } from '@/app/ui/button';
 import {
   CheckIcon,
   ClockIcon,
@@ -8,8 +10,7 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { Button } from '@/app/ui/button';
-import { updateInvoice } from '@/app/lib/actions';
+import { ChangeEvent, useActionState, useState } from 'react';
 
 export default function EditInvoiceForm({
   invoice,
@@ -18,10 +19,20 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const initialState: State = { message: null, errors: {} };
+  const { id, customer_id, amount: invoiceAmount, status } = invoice;
+  const updateInvoiceWithId = updateInvoice.bind(null, id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+  const [amount, setAmount] = useState(invoiceAmount);
+
+  const handleChangeAmount = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = Number(e.target.value);
+
+    setAmount(value);
+  };
 
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <div className='rounded-md bg-gray-50 p-4 md:p-6'>
         {/* Customer Name */}
         <div className='mb-4'>
@@ -33,14 +44,14 @@ export default function EditInvoiceForm({
               id='customer'
               name='customerId'
               className='peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500'
-              defaultValue={invoice.customer_id}
+              defaultValue={customer_id}
             >
               <option value='' disabled>
                 Select a customer
               </option>
-              {customers.map((customer) => (
-                <option key={customer.id} value={customer.id}>
-                  {customer.name}
+              {customers.map(({ id, name }: CustomerField) => (
+                <option key={id} value={id}>
+                  {name}
                 </option>
               ))}
             </select>
@@ -60,11 +71,21 @@ export default function EditInvoiceForm({
                 name='amount'
                 type='number'
                 step='0.01'
-                defaultValue={invoice.amount}
+                value={amount}
+                onChange={handleChangeAmount}
                 placeholder='Enter USD amount'
                 className='peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500'
+                aria-describedby='amount-error'
               />
               <CurrencyDollarIcon className='pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
+            </div>
+            <div id='amount-error' aria-live='polite' aria-atomic='true'>
+              {state.errors?.amount &&
+                state.errors.amount.map((error: string) => (
+                  <p className='mt-2 text-sm text-red-500' key={error}>
+                    {error}
+                  </p>
+                ))}
             </div>
           </div>
         </div>
@@ -82,7 +103,7 @@ export default function EditInvoiceForm({
                   name='status'
                   type='radio'
                   value='pending'
-                  defaultChecked={invoice.status === 'pending'}
+                  defaultChecked={status === 'pending'}
                   className='h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2'
                 />
                 <label
@@ -98,7 +119,7 @@ export default function EditInvoiceForm({
                   name='status'
                   type='radio'
                   value='paid'
-                  defaultChecked={invoice.status === 'paid'}
+                  defaultChecked={status === 'paid'}
                   className='h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2'
                 />
                 <label
@@ -111,6 +132,11 @@ export default function EditInvoiceForm({
             </div>
           </div>
         </fieldset>
+        <div id='form-error' aria-live='polite' aria-atomic='true'>
+          {state.message && (
+            <p className='mt-2 text-sm text-red-500'>{state.message}</p>
+          )}
+        </div>
       </div>
       <div className='mt-6 flex justify-end gap-4'>
         <Link
